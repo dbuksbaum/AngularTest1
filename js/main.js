5d0b477fc0a8df65a53c17f78cb375697f3b1497
David Buksbaum <david.buksbaum@gmail.com>
2013-10-20 20:19:50 -0400

    egghead.io AngularJS Tutorial # 4

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 /**
  * Created by david on 10/20/13.
  */
+var myApp = angular.module('myApp', []);
+myApp.factory('Data', function() {
+  "use strict";
+  return { message: "I'm data from a service"}
+})
 
 // the method name matches the controller name in index.html
-function FirstCtrl($scope){
+function FirstCtrl($scope, Data){
   "use strict";
+  $scope.data = Data;
 }
 
-function SecondCtrl($scope){
+function SecondCtrl($scope, Data){
   "use strict";
-
+  $scope.data = Data;
 }
